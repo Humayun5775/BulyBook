1aa2d083878f44fe7ff2915b436f00734957a572
humayun5775 <humayunobaid99@gmail.com>
2023-02-22 18:25:06 +0400

    Add project files.

---
+++ BulyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulyBookWeb.Data;
+using BulyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BulyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using BulyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulyBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
