6da1c662e679407ff3eb7646ab0f26d07ef90c25
humayun5775 <humayunobaid99@gmail.com>
2023-02-26 15:18:12 +0400

    new 2

--- BulyBookWeb/Controllers/CategoryController.cs
+++ BulyBookWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulyBookWeb.Data;
 using BulyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BulyBookWeb.Controllers
 {
@@ -18,9 +19,98 @@ namespace BulyBookWeb.Controllers
             return View(objCategoryList);
         }
 
+        //Get
         public IActionResult Create()
         {
             return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name  == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+        return View(obj);
+        }
+    
+
+    //Get
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        if(categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+
+        //Get
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+
+            if (obj == null)
+            {
+               
+                return NotFound();
+
+            }
+            _db.Categories.Remove(obj);
+                _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ BulyBookWeb/Views/Category/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulyBookWeb.Views.Category
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
